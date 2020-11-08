346b43aaff16b096e28fdffd4259e9c6befd0ae1
Jorgedel002 <71532870+Jorgedel002@users.noreply.github.com>
2020-11-08 15:55:47 -0300

    LBA Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace La_boutique_del_anteojo_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
